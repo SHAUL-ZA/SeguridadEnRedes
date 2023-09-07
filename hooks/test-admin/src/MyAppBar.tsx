109c434afbd3839698c5d61a75dc959094c65003
sergiozuckermann <a01024831@itesm.mx>
2023-09-07 11:44:09 -0600

    Added Idle hook

--- hooks/test-admin/src/MyAppBar.tsx
+++ hooks/test-admin/src/MyAppBar.tsx
@@ -1,9 +1,21 @@
 // in src/MyAppBar.tsx
 import { AppBar } from 'react-admin';
 import { ToggleThemeButton, useNotify } from 'react-admin';
+import { Card, CardContent, CardHeader } from "@mui/material";
+import { useIdle } from "@uidotdev/usehooks";
 
 
-export const MyAppBar = () => (
-    <AppBar toolbar={<ToggleThemeButton />} />
-);
+
+export function MyAppBar(){
+    const idle = useIdle(5000);
+    return (
+    <AppBar toolbar={<ToggleThemeButton />}>
+        <span className={idle ? "idle" : ""}/>
+        <CardContent>{idle ? "Inactivo" : "Activo"}</CardContent>
+    </AppBar>
+    )
+    ;
+    
+    
+    };
  
