a3133353e2663ed991b17c45e637cb721b065436
sergiozuckermann <a01024831@itesm.mx>
2023-10-04 18:33:11 -0600

    Added early login design

--- sistema/test-admin/src/MyLoginPage.tsx
+++ sistema/test-admin/src/MyLoginPage.tsx
@@ -2,40 +2,60 @@
 import * as React from "react";
 import { useState } from "react";
 import { useLogin, useNotify, Notification } from "react-admin";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Box } from "@mui/material";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const MyLoginPage = ({}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const login = useLogin();
   const notify = useNotify();
-
+  const size = useWindowSize();
+  const boxwidth= (size.width/4)*3;
+  const boxheight= (size.height/3)*2;
+  const logowidth= (size.width/12)*4.5;
+  const logoheight= size.height/3;
   const handleSubmit = (e) => {
+    
     e.preventDefault();
-    // will call authProvider.login({ email, password })
     login({ email, password }).catch(() => notify("Invalid email or password"));
   };
 
   return (
-    <Container maxWidth="xl">
-      <form onSubmit={handleSubmit}>
+    <Box sx={{ width: size.width, height: size.height, background: '#fff', }} >
+      
+      <form style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)"}} onSubmit={handleSubmit} autoComplete="on" >
+        <Box sx={{ width: boxwidth, height: boxheight, boxShadow: 3 }}>
+        <Box sx={{ width: logowidth, height: logoheight}}>
+        <img src= "../recursos/Logo.png" alt="Logo Fundación por México" width={logowidth} height={logoheight}></img>
+        </Box>
         <input
+          placeholder="Usuario"
           name="email"
           type="email"
           value={email}
+          autoComplete="on"
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
         <input
+          placeholder="Constraseña"
           name="password"
           type="password"
           value={password}
+          //width="48" 
+          //height="48"
+          autoComplete="on"
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
         <Button variant="contained" onClick={handleSubmit}>
           Login
         </Button>
+        </Box>
       </form>
-    </Container>
+      </Box>
+      
   );
 };
 
