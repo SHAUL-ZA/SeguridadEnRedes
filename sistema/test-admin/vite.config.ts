41865014c756e36ccc1f30e241b05ff16975ea2d
Dav <dm39988@gmail.com>
2023-10-11 22:28:33 -0600

    vite config fix

--- sistema/test-admin/vite.config.ts
+++ sistema/test-admin/vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
-const options = {
-    key: fs.readFileSync('./frontend.key'),
-    cert: fs.readFileSync('./frontend.cer'),
-}
+// const options = {
+//     key: fs.readFileSync('./frontend.key'),
+//     cert: fs.readFileSync('./frontend.cer'),
+// }
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     define: {
+//         'process.env': process.env,
+//     },
+//     server: {
+//         https: options,
+//     },
+//     base: './',
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +26,7 @@ export default defineConfig({
         'process.env': process.env,
     },
     server: {
-        https: options,
+        host: true,
     },
     base: './',
 });
