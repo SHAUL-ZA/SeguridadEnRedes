9d22365bd8290ed9b78589c808ffcb15f602af00
Shaul Zayat <shaulzayat1@gmail.com>
2023-10-12 15:42:06 -0600

    Delete usless files

--- test-admin/src/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-// in src/Dashboard.tsx
-import { Card, CardContent, CardHeader } from "@mui/material";
-
-export const Dashboard = () => (
-    <Card>
-        <CardHeader title="Bienvenido a la administracion" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-);
